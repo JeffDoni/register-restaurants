66fe78b3f2d771fb1bd75b755571b212af40d92d
Jefferson Donizete do Nascimento <jdn1937@hotmail.com>
2023-05-31 16:33:35 -0300

    alterando index

--- back-end/index.ts
+++ back-end/index.ts
@@ -9,7 +9,9 @@ import restauranteRoutes from './src/routes/restauranteRoutes';
 import horarioRoutes from './src/routes/horarioRoutes';
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 
 app.use(bodyParser.json());
 
